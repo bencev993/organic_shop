e78574caa7e76477a88293d2085d9f538aeabf5d
bencev93 <velmovszkibence@gmail.com>
2021-11-02 20:55:29 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,30 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        blue: {
+          light: '#EAF4FB',
+          medium: '#3498DB'
+        },
+        gray: {
+          light: '#F8F8F8'
+        },
         green: {
           light: '#76A713',
           dark: '#1f5c0c',
           hover: '#8BB538'
-        } 
+        },
+        orange: {
+          500: '#FF6600'
+        },
+        salmon: {
+          light: '#FFEFE5'
+        }
+      },
+      inset: {
+        '18': '4.5rem'
+      },
+      zIndex: {
+        '-10': '-10',
       },
     },
   },
