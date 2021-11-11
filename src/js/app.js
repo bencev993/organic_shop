76f21736888a88c35b971b3394d18cb01c2a7a62
bencev93 <velmovszkibence@gmail.com>
2021-11-11 11:40:57 +0100

    menu & search

---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+const menuBtn = document.querySelector('#toggleMenu');
+const searchIcon = document.querySelectorAll('.search-icon');
+const navItems = document.querySelectorAll('.nav-li');
+const searchContainer = document.querySelector('.search-container');
+const searchInput = document.querySelector('.search-input');
+let toggleM = true;
+let toggleS = true;
+
+menuBtn.addEventListener('click', () => { toggleMenu(); })
+
+searchIcon.forEach((icon) => {
+    icon.addEventListener('click', () => { toggleSearch(); })
+})
+
+searchContainer.addEventListener('click', (e) => {
+    !searchContainer.firstElementChild.contains(e.target) ? toggleSearch() : null;
+})
+
+window.addEventListener('keydown', (e) => {
+    e.key = 'Escape' && toggleS ? toggleSearch() : ''; 
+})
+
+function toggleMenu() {
+    searchContainer.classList.add('hidden'); 
+    navItems.forEach((item) => {
+        if(toggleM) {
+            item.classList.remove('hidden');
+            window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        } else {
+            item.classList.add('hidden');
+        }
+    });
+    toggleM = !toggleM;
+}
+
+function toggleSearch() {
+    if(toggleS) {
+        searchContainer.classList.toggle('hidden');
+        navItems.forEach((item) => {
+            item.classList.add('hidden');
+        });
+        toggleS = false;
+    } else {
+        searchContainer.classList.add('hidden');
+        searchInput.value = '';
+        toggleS = true;
+    }
+}
